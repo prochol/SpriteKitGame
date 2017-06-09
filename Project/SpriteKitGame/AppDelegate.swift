7b3d35b40bae84f1c8180afea35a72cffef47fc5
Pavel Kuzmin <prochol@ya.ru>
2017-06-09 15:02:25 +0700

    added start project

---
+++ Project/SpriteKitGame/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SpriteKitGame
+//
+//  Created by Kuzmin on 09.06.17.
+//  Copyright © 2017 Kuzmin. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
