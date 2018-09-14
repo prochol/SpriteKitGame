f4779ac9f7e0c9cadf8e9f48192d3e68da3932b9
prochol <prochol@ya.ru>
2018-09-14 11:14:21 +0700

    added account for cursor shift when dragging shapes

--- Project/SpriteKitGame/TetraminoScene.swift
+++ Project/SpriteKitGame/TetraminoScene.swift
@@ -15,6 +15,7 @@ class TetraminoScene: SKScene {
     private var rightTileMap: SKTileMapNode!
     
     private var selectedFigure: FigureNode?
+    private var selectedOfset: CGPoint?
     
     private var mapScale: CGPoint!
     
@@ -24,23 +25,20 @@ class TetraminoScene: SKScene {
         let figureSize = FigureNode.kBaseFigureSize
         
         var figure = FigureNode.figureOFill()
-        
         figure.scale(to: CGSize(width: figure.size.width * mapScale.x, height: figure.size.height * mapScale.y))
         figure.position = CGPoint(x: self.leftTileMap.position.x - figureSize.width / 2 * mapScale.x, y: self.leftTileMap.position.y - figureSize.height / 2 * mapScale.y)
         self.addChild(figure)
         
         figure = FigureNode.figureJ()
-        figure.scale(to: CGSize(width: figure.size.width *  mapScale.x, height: figure.size.height * mapScale.y))
+        figure.scale(to: CGSize(width: figure.size.width * mapScale.x, height: figure.size.height * mapScale.y))
         figure.position = CGPoint(x: self.leftTileMap.position.x - figureSize.width * 3 / 2 * mapScale.x, y: self.leftTileMap.position.y + figureSize.height * mapScale.y)
         figure.zRotation = CGFloat.pi
-        
         self.addChild(figure)
         
         figure = FigureNode.figureS()
-        figure.scale(to: CGSize(width: figure.size.width * mapScale.y, height: figure.size.height *  mapScale.x))
+        figure.scale(to: CGSize(width: figure.size.width * mapScale.y, height: figure.size.height * mapScale.x))
         figure.position = CGPoint(x: self.leftTileMap.position.x + figureSize.width * 3 / 2 * mapScale.x, y: self.leftTileMap.position.y)
         figure.zRotation = CGFloat.pi/2
-        
         self.addChild(figure)
     }
     
@@ -78,22 +76,29 @@ class TetraminoScene: SKScene {
             }
         }
         
-        self.selectedFigure?.zPosition = 1
-        if let selectedSize = self.selectedFigure?.size {
-            self.selectedFigure?.scale(to: CGSize(width: selectedSize.width * 1.25, height: selectedSize.height * 1.25))
+        if let selectedFigure = self.selectedFigure {
+            self.selectedOfset = CGPoint.init(x: selectedFigure.position.x - point.x, y: selectedFigure.position.y - point.y)
+            
+            selectedFigure.zPosition = 1
+            
+            let selectedSize = selectedFigure.size
+            selectedFigure.scale(to: CGSize(width: selectedSize.width * 1.25, height: selectedSize.height * 1.25))
         }
     }
     
     private func touchMoved(to point: CGPoint) {
-        self.selectedFigure?.position = point
+        self.selectedFigure?.position = CGPoint.init(x: point.x + self.selectedOfset!.x, y: point.y + self.selectedOfset!.y)
     }
     
     private func touchUp(at point: CGPoint) {
-        if let selectedSize = self.selectedFigure?.size {
-            self.selectedFigure?.scale(to: CGSize(width: selectedSize.width * 0.8, height: selectedSize.height * 0.8))
+        if let selectedFigure = self.selectedFigure {
+            let selectedSize = selectedFigure.size
+            selectedFigure.scale(to: CGSize(width: selectedSize.width * 0.8, height: selectedSize.height * 0.8))
+            
+            selectedFigure.zPosition = 0
+            
+            self.selectedOfset = nil
+            self.selectedFigure = nil
         }
-        self.selectedFigure?.zPosition = 0
-        
-        self.selectedFigure = nil
     }
 }
